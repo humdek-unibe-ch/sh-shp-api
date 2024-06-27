d995ac1ed6e1ccddfbebb4a50e53a852cceeada0
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2024-06-27 11:44:13 +0200

    rename internal and exteranl tables api calls

--- server/db/v1.1.0.sql
+++ server/db/v1.1.0.sql
@@ -4,4 +4,25 @@ SET version = 'v1.1.0'
 WHERE `name` = 'api';
 
 DELETE FROM pages
-WHERE keyword IN ('api_get_internal','api_get_internal_all');
+WHERE keyword IN ('api_get_internal','api_get_internal_all','api_import_external_row');
+
+UPDATE pages
+SET keyword = 'api_get_data', url = '/api/[data:class]/[table:method]/[v:table_name]'
+WHERE keyword = 'api_get_external_all';
+
+UPDATE pages
+SET keyword = 'api_get_my_data', url = '/api/[my:userMode]/[data:class]/[table:method]/[v:table_name]'
+WHERE keyword = 'api_get_external';
+
+UPDATE pages
+SET keyword = 'api_add_data', url = '/api/[data:class]/[table:method]/[v:table_name]'
+WHERE keyword = 'api_import_external';
+
+UPDATE pages
+SET keyword = 'api_update_data', url = '/api/[data:class]/[table:method]/[v:table_name]'
+WHERE keyword = 'api_update_external_row';
+
+UPDATE pages
+SET keyword = 'api_create_dataTable', url = '/api/[data:class]/[table:method]'
+WHERE keyword = 'api_create_external_table';
+
